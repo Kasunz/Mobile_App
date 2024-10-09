96acc0cf775d703dc9985f3b4c2eb8d4bb8fcea6
Kasun-Madusanka-Bandara <kasunmadhusankayt@gmail.com>
2024-10-09 22:11:00 +0530

    save student details using a button with print output.

--- app.py
+++ app.py
@@ -7,19 +7,30 @@ from kivy.uix.button import Button
 
 class ChildApp(GridLayout):
     def __init__(self, **kwargs):
-        super(ChildApp, self).__init__()
+        super(ChildApp, self).__init__(**kwargs)
         self.cols = 2
         self.add_widget(Label(text = 'Student Name: '))
-        self.s_name = TextInput()
+        self.s_name = TextInput(size_hint=(0.7, 0.1))
         self.add_widget(self.s_name)
 
         self.add_widget(Label(text = 'Student Marks: '))
-        self.s_marks = TextInput()
+        self.s_marks = TextInput(size_hint=(0.7, 0.1))
         self.add_widget(self.s_marks)
 
         self.add_widget(Label(text = 'Student Gender: '))
-        self.s_gender = TextInput()
+        self.s_gender = TextInput(size_hint=(0.7, 0.1))
         self.add_widget(self.s_gender)
+
+        self.press = Button(text = "Save")
+        self.press.bind(on_press = self.click_me)
+        self.add_widget(self.press)
+
+    def click_me(self, instance):
+        print("Name of Student is " +self.s_name.text)
+        print("Marks of Student is " +self.s_marks.text)
+        print("Gender of Student is " + self.s_gender.text)
+        print("")
+
 
 class ParentApp(App):
     def build(self):
